341866d72b0c2cf130b6cfba289c4b8ef46cc464
Caroline Berg <carolineeberg@gmail.com>
2018-07-07 20:25:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  row_one = [" #{board[0]} | #{board[1]} | #{board[2]} "]
+  row_two = [" #{board[3]} | #{board[4]} | #{board[5]} "]
+  row_three = [" #{board[6]} | #{board[7]} | #{board[8]} "]
+  barrier = ["-----------"]
+  puts row_one
+  puts barrier
+  puts row_two
+  puts barrier
+  puts row_three
+end
