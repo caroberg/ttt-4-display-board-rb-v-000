c79b56367afa3a342a4ef0a88bef956b0ed3f5f6
Caroline Berg <carolineeberg@gmail.com>
2018-07-07 21:18:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,16 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board_two = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board_three = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+board_four = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board_five = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board_six = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board_seven = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+board_eight = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+board_nine = [" ", "X", "O", "O", "X", " ", " ", "O", "X"]
+board_ten = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+board_eleven = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
 def display_board(board)
-  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
   row_one = [" #{board[0]} | #{board[1]} | #{board[2]} "]
   row_two = [" #{board[3]} | #{board[4]} | #{board[5]} "]
   row_three = [" #{board[6]} | #{board[7]} | #{board[8]} "]
